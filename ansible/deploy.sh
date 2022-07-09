6c2228b4fb5bfc22664e54c3ba8996a0f93a3cbc
cmartinUnir <104629214+cmartinUnir@users.noreply.github.com>
2022-07-09 11:14:01 +0200

    Update deploy.sh

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Ejecutamos el playbook de despliegue de Kubernetes
+
 ansible-playbook -i hosts despliegue-kubernetes.yml
 
-# Ejecutamos el playbook de despliegue de la aplicación
-ansible-playbook -i hosts despliegue-aplicacion.yml
+
